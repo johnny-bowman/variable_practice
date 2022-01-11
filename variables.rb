fa6b328e44445269bdce42ff398424e3afbb2ee1
Johnny Bowman <johntbowman.production@gmail.com>
2022-01-10 20:15:06 -0700

    Add booleans

--- variables.rb
+++ variables.rb
@@ -1,4 +1,7 @@
 climbing = "My favorite activity"
 swimming = "My least favorite activity"
 years_climbing = 2.5
-yosemite_multipitch = 1
+yosemite_epics = 1
+
+climbing_is_dangerous = true
+driving_is_dangerous = true
