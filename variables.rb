a691fc0f1e364fed2a49e9dac913300418451796
Johnny Bowman <johntbowman.production@gmail.com>
2022-01-10 20:19:25 -0700

    Annotate strings

--- variables.rb
+++ variables.rb
@@ -1,5 +1,8 @@
+# variable climbing is declared and assigned to the string of "My favorite activity"
 climbing = "My favorite activity"
+# variable swimming is declared and assigned to the string of "My least favorite activity"
 swimming = "My least favorite activity"
+
 years_climbing = 2.5
 yosemite_epics = 1
 
